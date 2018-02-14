e9ca0f8d2b25239a2a21002e6d2ae60f17c4ca8a
Unknown <norkasrom@gmail.com>
2018-02-14 14:04:13 -0700

    1

---
+++ ProyectoCRM/AutoEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProyectoCRM
+{
+    public class AutoEntity
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public int Modelo { get; set; }
+        public string Tipo { get; set; }
+        public string Color { get; set; }
+
+    }
+}
